cb0b054189bc412ad4fda3db26d070840b845217
dungdaaa <dungdaaa123@gmail.com>
2023-11-23 11:35:32 +0900

    detailSV desc

--- App.js
+++ App.js
@@ -50,7 +50,6 @@ export default function App() {
     getWeather();
   }, []);
 
-  ///////////////////////////time
   //date
   const [currentDate, setCurrentDate] = useState(new Date());
 
@@ -72,7 +71,6 @@ export default function App() {
 
     return () => clearInterval(timeInterval);
   }, []);
-  ///////////////////////////time
 
   const getAirQualityStatus = () => {
     const pm10 = currentAir.pm10;
@@ -87,6 +85,23 @@ export default function App() {
       return "나쁨";
     } else {
       return "매우나쁨";
+    }
+  };
+
+  //Image Description Detail
+  const getAirQualityStatusDescription = () => {
+    const pm10 = currentAir.pm10;
+
+    if(pm10 <= 20){
+      return "설명";
+    } else if(pm10 <= 50){
+      return "설명";
+    } else if(pm10 <= 100){
+      return "설명";
+    } else if(pm10 <= 200){
+      return "설명";
+    } else {
+      return "설명";
     }
   };
 
@@ -105,6 +120,108 @@ export default function App() {
       return require('./assets/mise_verybad.png');
     }
   };
+
+  //미세먼지 pm10
+  const getPm10_SV = () => {
+    const pm10 = currentAir.pm10;
+
+    if(pm10 <= 20){
+      return "매우좋음";
+    } else if(pm10 <= 50){
+      return "좋음";
+    } else if(pm10 <= 100){
+      return "보통";
+    } else if(pm10 <= 200){
+      return "나쁨";
+    } else{
+      return "매우나쁨";
+    }
+  };
+
+  //초미세먼지 pm25
+  const getPm25_SV = () => {
+    const pm25 = currentAir.pm25;
+
+    if(pm25 <= 10){
+      return "매우좋음";
+    } else if(pm25 <= 25){
+      return "좋음";
+    } else if(pm25 <= 50){
+      return "보통";
+    } else if(pm25 <= 75){
+      return "나쁨";
+    } else{
+      return "매우나쁨";
+    }
+  };
+
+  //이산화질소 no
+  const getNo2_SV = () => {
+    const no2 = currentAir.no2;
+
+    if(no2 <= 40){
+      return "매우좋음";
+    } else if(no2 <= 70){
+      return "좋음";
+    } else if(no2 <= 150){
+      return "보통";
+    } else if(no2 <= 200){
+      return "나쁨";
+    } else{
+      return "매우나쁨";
+    }
+  };
+
+  //오존 o3
+  const getO3_SV = () => {
+    const o3 = currentAir.o3;
+
+    if(o3 <= 60){
+      return "매우좋음";
+    } else if(o3 <= 100){
+      return "좋음";
+    } else if(o3 <= 140){
+      return "보통";
+    } else if(o3 <= 180){
+      return "나쁨";
+    } else{
+      return "매우나쁨";
+    }
+  };
+
+  //일산화탄소 co
+  const getCo_SV = () => {
+    const co = currentAir.co;
+
+    if(co <= 4400){
+      return "매우좋음";
+    } else if(co <= 9400){
+      return "좋음";
+    } else if(co <= 12400){
+      return "보통";
+    } else if(co <= 15400){
+      return "나쁨";
+    } else{
+      return "매우나쁨";
+    }
+  }
+
+  //아황산가스 so2
+  const getSo2_SV = () => {
+    const so2 = currentAir.so2;
+
+    if(so2 <= 20){
+      return "매우좋음";
+    } else if(so2 <= 80){
+      return "좋음";
+    } else if(so2 <= 250){
+      return "보통";
+    } else if(so2 <= 350){
+      return "나쁨";
+    } else{
+      return "매우나쁨";
+    }
+  }
 
   return (
     <View style = {styles.container}>
@@ -140,26 +257,32 @@ export default function App() {
           >
             <View style = {styles.detail}>
               <Text style = {styles.detailTitle}>미세먼지</Text>
+              <Text>{getPm10_SV()}</Text>
               <Text style = {styles.detailValue}>{currentAir.pm10}</Text>
             </View>
             <View style = {styles.detail}>
               <Text style = {styles.detailTitle}>초미세먼지</Text>
+              <Text>{getPm25_SV()}</Text>
               <Text style = {styles.detailValue}>{currentAir.pm25}</Text>
             </View>
             <View style = {styles.detail}>
               <Text style = {styles.detailTitle}>이산화질소</Text>
+              <Text>{getNo2_SV()}</Text>
               <Text style = {styles.detailValue}>{currentAir.no2}</Text>
             </View>
             <View style = {styles.detail}>
               <Text style = {styles.detailTitle}>오존</Text>
+              <Text>{getO3_SV()}</Text>
               <Text style = {styles.detailValue}>{currentAir.o3}</Text>
             </View>
             <View style = {styles.detail}>
               <Text style = {styles.detailTitle}>일산화탄소</Text>
+              <Text>{getCo_SV()}</Text>
               <Text style = {styles.detailValue}>{currentAir.co}</Text>
             </View>
             <View style = {styles.detail}>
               <Text style = {styles.detailTitle}>아황산가스</Text>
+              <Text>{getSo2_SV()}</Text>
               <Text style = {styles.detailValue}>{currentAir.so2}</Text>
             </View>
           </ScrollView>
@@ -175,7 +298,7 @@ const styles = StyleSheet.create({
   },
   top: {
     flex: 2.5,
-    backgroundColor: "#fafafa",
+    //backgroundColor: "#fafafa",
   },
   city: {
     flex: 8,
@@ -200,30 +323,31 @@ const styles = StyleSheet.create({
 
   mid: {
     flex: 5.5,
-    backgroundColor: "#fafafa",
+    //backgroundColor: "#fafafa",
   },
   image: {
     flex: 8,
-    backgroundColor: "green",
+    //backgroundColor: "green",
     justifyContent: "center",
     alignItems: "center",
   },
   description: {
     flex: 2,
-    backgroundColor: "yellow",
+    //backgroundColor: "yellow",
     justifyContent: "center",
     alignItems: "center",
+    //크기 조절
   },
 
   bot: {
     flex: 2,
-    backgroundColor: "#fafafa",
+    //backgroundColor: "#fafafa",
   },
   SV: {
 
   },
   detail: {
-    backgroundColor: "blue",
+    //backgroundColor: "blue",
     justifyContent: "center",
     alignItems: "center",
     width: SCREEN_WIDTH / 3,
